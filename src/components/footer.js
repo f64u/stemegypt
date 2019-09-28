83e50be9561a6226227f5aea24430d79b7bab8bd
Fady Adel <2masadel@gmail.com>
2019-09-28 11:22:14 +0200

    added footer - still not responsive

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,50 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import styles from "./footer.module.scss"
 
+import logo from "../assets/logo.svg"
+import mirus from "../assets/icons/mirus.svg"
+
 const Footer = () => {
   return (
-    <header className={styles.footer}>
-      <h1>This is the footer</h1>
-    </header>
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <ul className={styles.navFooter}>
+          <li className={styles.navLink}>
+            <Link to="/">Home</Link>
+          </li>
+          <li className={styles.navLink}>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+          <li className={styles.navLink}>
+            <Link to="/request-info">Request Information</Link>
+          </li>
+          <li className={styles.navLink}>
+            <Link to="/about/policy">Privacy Policy</Link>
+          </li>
+          <li className={`${styles.navLink} ${styles.last}`}>
+            <Link to="/about/map">Site Map</Link>
+          </li>
+        </ul>
+
+        <div className={styles.logo}>
+          <img src={logo} alt="STEM Egypt" width={85} />
+        </div>
+        <h2 className={styles.title}>
+          STEM High School for Boys, 6th of October
+        </h2>
+        <h3 className={styles.copyNotice}>
+          &copy; 2011-2019 All Rights Reserved by STEM Egypt Schools
+        </h3>
+
+        <div className={styles.designNotice}>
+          {/* Will this really last? */}
+          <h3>Designed by</h3>
+          <img src={mirus} alt="mirus" width={50} />
+        </div>
+      </div>
+    </footer>
   )
 }
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,11 +2,15 @@ import React from "react"
 import Header from "./header"
 import Footer from "./footer"
 
+import styles from "./layout.module.scss"
+
 const Layout = ({ children }) => {
   return (
-    <div>
-      <Header />
-      {children}
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <Header />
+        {children}
+      </div>
       <Footer />
     </div>
   )
