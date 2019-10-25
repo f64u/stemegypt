1b24b1930711ce0b7e2c71c941f8c114501b7474
Fady Adel <2masadel@gmail.com>
2019-10-25 19:07:52 +0200

    finalized meet our alumni

--- src/pages/alumni/meet.js
+++ src/pages/alumni/meet.js
@@ -1,13 +1,22 @@
 import React from "react"
+
+import alumniImage from "../../assets/landings/alumni.jpg"
+
 import Layout from "../../components/shared/layout"
 import SiteMetadata from "../../components/shared/site-metadata"
 import Paragraphs from "../../components/meetalumni/paragraphs"
+import LandingImage from "../../components/shared/landingimage"
 
 const MeetAlumni = () => {
   return (
     <Layout>
       <SiteMetadata pathname="/alumni/meet" pageTitle="Meet our Alumni" />
 
+      <LandingImage
+        imgSrc={alumniImage}
+        title="Meet our Alumni"
+        subtitle="Stories about two of our alumni"
+      />
       <Paragraphs />
     </Layout>
   )
